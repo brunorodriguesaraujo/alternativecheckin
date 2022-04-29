adc73af7b3041bfa59f38939c5ddeb910b9a6a61
Bruno Rodrigues <brunorodrigues-78@hotmail.com>
2022-04-28 23:15:37 -0300

    feat: #main add email

--- app/src/main/java/br/com/alternativecheck_in/model/Driver.kt
+++ app/src/main/java/br/com/alternativecheck_in/model/Driver.kt
@@ -2,16 +2,15 @@ package br.com.alternativecheck_in.model
 
 import android.os.Parcelable
 import br.com.alternativecheck_in.helper.FirebaseHelper
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.parcelize.Parcelize
 import java.io.Serializable
 
 @Parcelize
 data class Driver(
     var id: String = "",
-    var driverCod: Int? = null,
+    var driverCod: String = "",
     var driverName: String = "",
-): Parcelable, Serializable {
-    init {
-        this.id = FirebaseHelper.getDatabase().push().key ?: ""
-    }
-}
+    var driverEmail: String = ""
+) : Parcelable, Serializable
+
